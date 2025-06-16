57462385e28c47219d6aff028230f9ac5b8be6b5
gapima <gapima7@gmail.com>
2025-06-16 16:36:19 -0300

    ref main.c

--- src/main.c
+++ src/main.c
@@ -6,20 +6,34 @@
 /*   By: glima <glima@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 00:27:03 by glima             #+#    #+#             */
-/*   Updated: 2025/06/14 19:19:23 by glima            ###   ########.fr       */
+/*   Updated: 2025/06/16 16:35:22 by glima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	show_usage(void)
+{
+	ft_putstr_fd("Uso: ./cub3D <mapa.cub>\n", 2);
+	exit(EXIT_FAILURE);
+}
+
+static void	start_loop(t_config *cfg)
+{
+	mlx_loop_hook(cfg->mlx, &hook, cfg);
+	mlx_key_hook(cfg->mlx, &handle_input, cfg);
+	mlx_loop(cfg->mlx);
+	mlx_terminate(cfg->mlx);
+}
+
 void	hook(void *param)
 {
-	t_config *cfg = (t_config *)param;
+	t_config	*cfg;
 
+	cfg = (t_config *)param;
 	mlx_delete_image(cfg->mlx, cfg->img);
 	cfg->img = mlx_new_image(cfg->mlx, WIDTH, HEIGHT);
 	mlx_image_to_window(cfg->mlx, cfg->img, 0, 0);
-
 	render_frame(cfg);
 }
 
@@ -28,18 +42,10 @@ int	main(int argc, char **argv)
 	t_config	cfg;
 
 	if (argc != 2)
-	{
-		puts("Uso: ./cub3D <mapa.cub>");
-		return (EXIT_FAILURE);
-	}
+		show_usage();
 	init_config(&cfg);
 	parse_cub_file(argv[1], &cfg);
-	load_textures(&cfg); // ⬅️ ADICIONE AQUI
-
-	mlx_loop_hook(cfg.mlx, &hook, &cfg);
-	mlx_key_hook(cfg.mlx, &handle_input, &cfg);
-	mlx_loop(cfg.mlx);
-	mlx_terminate(cfg.mlx);
+	load_textures(&cfg);
+	start_loop(&cfg);
 	return (EXIT_SUCCESS);
 }
-
