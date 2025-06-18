8b9ce206c962ec58dcb0b590d2dcbb8549c9145b
gapima <gapima7@gmail.com>
2025-06-18 16:53:20 -0300

    /home/klonoa

--- src/validate_map.c
+++ src/validate_map.c
@@ -6,41 +6,53 @@
 /*   By: glima <glima@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 15:46:50 by glima             #+#    #+#             */
-/*   Updated: 2025/06/18 00:02:32 by glima            ###   ########.fr       */
+/*   Updated: 2025/06/18 16:52:34 by glima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static bool check_diagonals(char **map, int x, int y, int height, int width)
+static bool	check_diagonals(char **map, int x, int y, int height, int width)
 {
-	if (y > 0 && x > 0 && (map[y - 1][x - 1] == ' ' || map[y - 1][x - 1] == 2))
-		return false;
-	if (y + 1 < height && x > 0 && (map[y + 1][x - 1] == ' ' || map[y + 1][x - 1] == 2))
-		return false;
-	if (y > 0 && x + 1 < width && (map[y - 1][x + 1] == ' ' || map[y - 1][x + 1] == 2))
-		return false;
-	if (y + 1 < height && x + 1 < width && (map[y + 1][x + 1] == ' ' || map[y + 1][x + 1] == 2))
-		return false;
-	return true;
+	if (
+		y > 0 && x > 0
+		&& (map[y - 1][x - 1] == ' ' || map[y - 1][x - 1] == 2)
+	)
+		return (false);
+	if (
+		y + 1 < height && x > 0
+		&& (map[y + 1][x - 1] == ' ' || map[y + 1][x - 1] == 2)
+	)
+		return (false);
+	if (
+		y > 0 && x + 1 < width
+		&& (map[y - 1][x + 1] == ' ' || map[y - 1][x + 1] == 2)
+	)
+		return (false);
+	if (
+		y + 1 < height && x + 1 < width
+		&& (map[y + 1][x + 1] == ' ' || map[y + 1][x + 1] == 2)
+	)
+		return (false);
+	return (true);
 }
 
-static bool check_sides(char **map, int x, int y, int height, int width)
+static bool	check_sides(char **map, int x, int y, int height, int width)
 {
 	if (y <= 0 || y >= height || x <= 0 || x >= width)
-		return false;
+		return (false);
 	if (y > 0 && (map[y - 1][x] == ' ' || map[y - 1][x] == 2))
-		return false;
+		return (false);
 	if (y + 1 < height && (map[y + 1][x] == ' ' || map[y + 1][x] == 2))
-		return false;
+		return (false);
 	if (x > 0 && (map[y][x - 1] == ' ' || map[y][x - 1] == 2))
-		return false;
+		return (false);
 	if (x + 1 < width && (map[y][x + 1] == ' ' || map[y][x + 1] == 2))
-		return false;
-	return true;
+		return (false);
+	return (true);
 }
 
-static void put_coord_error(int x, int y)
+static void	put_coord_error(int x, int y)
 {
 	ft_putstr_fd("Error: Map is not completely enclosed at coordinates (", 2);
 	ft_putnbr_fd(x, 2);
@@ -49,11 +61,11 @@ static void put_coord_error(int x, int y)
 	ft_putstr_fd(")\n", 2);
 }
 
-void validate_closed_map(char **map, int height, int width)
+void	validate_closed_map(char **map, int height, int width)
 {
-	int y;
-	int x;
-	char c;
+	int		y;
+	int		x;
+	char	c;
 
 	y = 0;
 	while (y < height)
